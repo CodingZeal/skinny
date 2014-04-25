0cb7648c9a1e973fea5e8eb0bfa672104fd24cda
Adam Cuppy <adam@codingzeal.com>
2014-04-25 07:35:29 -0700

    removing comments

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,6 @@ require 'spec_helper'
 
 describe 'user accounts' do
   describe "the length of the firstname" do
-    # TODO: all cases are invalid because of the password and not the name alone
-    # TODO: we never cover the other
 
     it "should not be super long" do
       user = User.new(firstname: "really really really really super super super long firstname that is crazy long")
@@ -11,7 +9,6 @@ describe 'user accounts' do
     end
 
     it "should not be super short" do
-      # TODO: later conflicts with Capitalization requirement
       user = User.new(firstname: "sht")
       user.valid?.should_not == true
     end
@@ -22,7 +19,6 @@ describe 'user accounts' do
     end
 
     it "shouldn't have weird characters" do
-      # TODO: later conflicts with password length
       user = User.new(firstname: "*&^&^$&")
       user.valid?.should_not == true
     end
@@ -31,17 +27,12 @@ describe 'user accounts' do
 
   describe "the user's email address" do
     it "should not accept an invalid format" do
-      # TODO: password fails when requirements change to add capitals and numbers
-      # TODO: permutation example
       user = User.new(email: "not email")
       user.valid?.should_not == true
     end
   end
 
   describe "updating the email address" do
-    # TODO: never tested that the email changed.  We've assumed that due to the valid? method returning true
-    # May fail when update_attribute silently discards the value
-    # May fail when requirements of the object change
     it "should still test the validation" do
       user = User.create(firstname: "Jimmy Jim", password: "password", email: "admin@example.com")
       user.valid?.should == true
@@ -52,7 +43,6 @@ describe 'user accounts' do
   end
 
   describe "password requirements" do
-    # TODO: Single expectation principle
     it "doesn't allow a passoword that's less than 8 characters and matches the firstname" do
       user = User.create(firstname: "Jimmy Jim", password: "passwrd", email: "admin@example.com")
       user.valid?.should_not == true
